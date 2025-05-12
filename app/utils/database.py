7a8833164236f209c78d0ea8b5ad744d400d9010
shajnimak <79740295+shajnimak@users.noreply.github.com>
2025-05-12 23:16:37 +0500

    fixes

--- app/utils/database.py
+++ app/utils/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL_ASYNC")
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 
