d432185df1f86a4563943a74bc7b71570569efa8
shajnimak <79740295+shajnimak@users.noreply.github.com>
2025-06-10 17:30:16 +0500

    port fixes

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ app = create_app()
 migrate = Migrate(app, db)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5001)))
+    app.run(host='0.0.0.0', port=5001)
